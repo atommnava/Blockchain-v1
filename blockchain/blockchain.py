c2a4daa732c1cbdd94f65cb3b224025d62646a86
atommnava <atommunoznava@gmail.com>
2025-05-20 09:01:27 -0600

    testing

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -6,3 +6,17 @@ class NeuralCoinBlock:
         self.transList = transList
 
         self.blockData = "-".join(transList) + "-" + prevBlockHash
+        # To calculate the hash
+        self.blockHash = hashlib.sha256(self.blockData.encode()).hexdigest()
+
+t1 = "Tom sends 2 NC to Mike"
+t2 = "Bob sends 4.2 NC to Mike"
+t3 = "Mike sends 3.2 NC to Bob"
+t4 = "Daniel sends 0.3 NC to Tom"
+t5 = "Mike sends 1 NC to Charlie"
+t6 = "Mike sends 5.2 NC to Daniel"
+
+initialBlock = NeuralCoinBlock("Initial String", [t1,t2])
+
+print(initialBlock.blockData)
+print(initialBlock.blockHash)
